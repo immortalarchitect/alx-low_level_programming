c43664955572427d988cb34e7f6285ab886af633
immortalarchitect <timothydistinction2@gmail.com>
2023-02-16 15:32:49 -0800

    advance

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entrry point
+ * Return:1
+ */
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Kopar, 2015-10-19\n", stdout);
+	return (1)
+}
