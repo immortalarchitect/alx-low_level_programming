e03d9c173804c95fcb59a541eebe6e773f2d3eea
immortalarchitect <timothydistinction2@gmail.com>
2023-02-16 16:09:38 -0800

    advance

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - Entry point
- * Return: 1 (Success)
+ * Return: 1 
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar\n", 2015-10-19");
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
